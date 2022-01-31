4631072a73019aacc39be0328b5b23089bb356a1
Alexey Erёmenko <n8240782@yandex.ru>
2022-01-31 23:37:08 +0300

    create 3 app: api, rewiews, users

---
+++ api_yamdb/api/urls.py
@@ -0,0 +1,8 @@
+from django.urls import include, path
+from rest_framework import routers
+
+router_v1 = routers.DefaultRouter()
+
+urlpatterns = [
+    # path(...),
+]
---
+++ api_yamdb/rewiews/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class RewiewsConfig(AppConfig):
+    name = 'rewiews'
