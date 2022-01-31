fb7bc76cdcf9d671c6f5cd8551bff1d629f92c7c
Alexey Erёmenko <n8240782@yandex.ru>
2022-02-01 01:38:01 +0300

    change users/models - add user_role

--- api_yamdb/users/models.py
+++ api_yamdb/users/models.py
@@ -3,4 +3,23 @@ from django.db import models
 
 
 class User(AbstractUser):
-    pass
+    USER = 'US'
+    MODERATOR = 'MD'
+    ADMIN = 'AD'
+
+    USER_ROLE = (
+        (USER, 'User'),
+        (MODERATOR, 'Moderator'),
+        (ADMIN, 'Administrator'),
+    )
+
+    role = models.CharField(
+        'Пользовательская роль',
+        max_length=10,
+        choices=USER_ROLE,
+        default=USER
+    )
+    bio = models.TextField(
+        'Биография',
+        blank=True,
+    )
