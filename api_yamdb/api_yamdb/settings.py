c1eab0e25889fba6b6f192f88eb1a5cc01eb5014
Alexey Erёmenko <n8240782@yandex.ru>
2022-02-01 00:10:49 +0300

    freeze requirements & small change users-app

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -16,6 +16,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'rest_framework_simplejwt',
     'rewiews',
     'users',
     'api',
@@ -87,3 +88,5 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static/'),)
+
+AUTH_USER_MODEL = 'users.User'
