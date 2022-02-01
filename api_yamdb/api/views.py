b14bf43a12960642add93ec1f84f799866a7005b
George Kryukov <k.viregor@gmail.com>
2022-02-01 16:25:31 +1100

    First changes(Zhora)

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -1,3 +1,28 @@
+from webbrowser import get
+from django.shortcuts import get_object_or_404
 from django.shortcuts import render
+from rest_framework import viewsets
+
+from rewiews.models import Review
 
 
+class ReviewViewSet(viewsets.ModelViewSet):
+    serializer_class = ReviewSerializer
+
+    def get_queryset(self):
+        title = get_object_or_404(Title, id=self.kwargs.get('title_id'))
+        return title.reviews.all()
+    
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
+
+
+class CommentViewSet(viewsets.ModelViewSet):
+    serializer_class = CommentSerializer
+
+    def get_queryset(self):
+        review = get_object_or_404(Review, id=self.kwargs.get('review_id'))
+        return review.comments.all()
+    
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
--- api_yamdb/rewiews/models.py
+++ api_yamdb/rewiews/models.py
@@ -1,1 +1,31 @@
 from django.db import models
+
+from users.models import User
+
+
+class Review(models.Model):
+    author = models.OneToOneField(
+        User, 
+        on_delete=models.CASCADE, 
+        related_name='reviews'
+    )
+    text = models.TextField()
+    title =  models.OneToOneField(
+        Title, 
+        on_delete=models.CASCADE, 
+        related_name='reviews'
+    )
+    name = models.CharField(max_length=100)
+
+
+class Comment(models.Model):
+    author = models.OneToOneField(
+        User, 
+        on_delete=models.CASCADE,
+        related_name='comments'
+    )
+    review = models.OneToOneField(
+        Review, on_delete=models.CASCADE, related_name='comments')
+    text = models.TextField()
+    created = models.DateTimeField(
+        'Дата добавления', auto_now_add=True, db_index=True)
