d3feb50059e3ef32330ea7dcc89f9d81fe5eefe0
Сергей Волков <mrvolkovsa@yandex.ru>
2022-02-03 16:45:22 +0300

    change api/views - add CategoryViewSet, GenreViewSet

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -1,12 +1,29 @@
-from rest_framework import viewsets
+from rest_framework import mixins, viewsets
 from rest_framework.pagination import LimitOffsetPagination
 
-from api.serializers import CategorySerializer
-from reviews.models import Category
+from api.serializers import CategorySerializer, GenreSerializer
+from reviews.models import Category, Genre
 
 
-class CategoryViewSet(viewsets.ModelViewSet):
+class ListCreateDestroyViewSet(mixins.ListModelMixin,
+                               mixins.CreateModelMixin,
+                               mixins.DestroyModelMixin,
+                               viewsets.GenericViewSet):
+    """Вьюсет для обработки запросов GET, POST и DELETE."""
+    pass
+
+
+class CategoryViewSet(ListCreateDestroyViewSet):
     """Управление категориями произведений."""
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
     pagination_class = LimitOffsetPagination
+    lookup_field = 'slug'
+
+
+class GenreViewSet(ListCreateDestroyViewSet):
+    """Управление категориями жанров."""
+    queryset = Genre.objects.all()
+    serializer_class = GenreSerializer
+    pagination_class = LimitOffsetPagination
+    lookup_field = 'slug'
